fa1f00f9cacc2384cf4b6a286eb7b7eae33d6579
Robson Lopes <robsonlopes@Robsons-MacBook-Pro.local>
2026-05-16 13:08:46 -0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  experimental: {
+    serverMinification: false,
+  },
 };
 
 export default nextConfig;
