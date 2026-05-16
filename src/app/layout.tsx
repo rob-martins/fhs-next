19d819c458f417d6ec6ecd6e083fa0021a3275fb
Rob Martins <eng.rob.martins@gmail.com>
2026-05-16 18:38:52 -0300

    adding brand style fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
 import "../styles/globals.css";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-main",
+});
 
 export const metadata: Metadata = {
   title: "Firehouse Subs",
@@ -12,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={montserrat.variable}>
       <body>{children}</body>
     </html>
   );
